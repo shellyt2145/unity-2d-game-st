59e5fb1547fc421dbe90ae71c05b02afd5a1ac84
shellyt2145 <55810536+shellyt2145@users.noreply.github.com>
2019-09-25 15:38:35 -0500

    yeet
    yeet

---
+++ YEET (2)/Assets/Scripts/Collectible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        handlecollected();
+        
+
+    }
+    void handlecollected()
+    {
+        CollectibleManager.Instance.handledankweedcollected();
+
+        print("420 Blaze it");
+        Destroy(gameObject);
+
+    }
+}
---
+++ YEET (2)/Assets/Scripts/CollectibleManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectibleManager : MonoBehaviour
+{
+    public static CollectibleManager Instance;
+
+
+    private int _totalDankWeed;
+    private int _collectedDankWeed;
+
+    public TextMeshProUGUI DankWeedCount;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        _totalDankWeed = FindObjectsOfType<Collectible>().Length;
+        DisplayDankWeedCount();
+
+    }
+
+    void DisplayDankWeedCount()
+    {
+        DankWeedCount.SetText(_totalDankWeed + " / " + _collectedDankWeed);
+    }
+
+    public void handledankweedcollected()
+    {
+        _collectedDankWeed++;
+        DisplayDankWeedCount();
+    }
+  
+  
+}
